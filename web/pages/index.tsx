21b2c594df0b40b343cdb9d6d2c44884a54e5c0e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-05-24 13:03:50 -0700

    add basic data

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,15 +1,54 @@
 import { NextPage } from "next";
+import { SITE_NAME } from "../constants";
 import { client } from "../lib/sanity";
 
+type ProjectData = {
+  date?: string;
+  images?: {}[];
+  location?: string;
+  roles?: string[];
+  title: string;
+};
+
 const Home: NextPage<{
-  data: {};
+  data?: ProjectData[];
 }> = ({ data }) => {
   console.log(data);
 
   return (
-    <div>
-      homepage here<pre>{JSON.stringify(data)}</pre>
-    </div>
+    <>
+      <aside>
+        <h1>{SITE_NAME}</h1>
+        <ul>
+          <li>Information</li>
+          <li>Index</li>
+        </ul>
+        <ul>
+          {data?.map((project) => (
+            <li>
+              {project.title} (
+              {project.location &&
+                project.date &&
+                `${project.location} ${new Date(project.date).getFullYear()}`}
+              )
+            </li>
+          ))}
+        </ul>
+      </aside>
+
+      <main>
+        <ul>
+          {data?.map((project) => (
+            <li>
+              {project.images?.map((image) => (
+                <div>image</div>
+              ))}
+              {project.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 };
 
