d531e5dff8fab2e59370e91c60695d9e0f0ee457
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-05-24 13:46:30 -0700

    update project ts

--- cms/schemas/project.ts
+++ cms/schemas/project.ts
@@ -1,5 +1,37 @@
 export default {
   type: "document",
   name: "project",
-  fields: [{ type: "string", name: "title" }],
+  fields: [
+    {
+      type: "string",
+      name: "title",
+    },
+
+    {
+      name: "location",
+      type: "string",
+      description: '"Austin, Texas" For example',
+    },
+
+    {
+      title: "Project Year",
+      name: "year",
+      type: "string",
+    },
+
+    {
+      title: "Project Roles / Deliverables",
+      name: "roles",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+
+    {
+      title: "Project Images",
+      name: "images",
+      type: "array",
+      options: { layout: "grid" },
+      of: [{ type: "image", options: { hotspot: true } }],
+    },
+  ],
 };
