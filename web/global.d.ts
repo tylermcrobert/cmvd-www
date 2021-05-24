ea6f34a753b213b49349d5c687bc2772b96b36bc
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-05-24 12:49:53 -0700

    remove unneded stuff

--- web/global.d.ts
+++ web/global.d.ts
@@ -1,5 +1,5 @@
-declare module '@sanity/block-content-to-html'
-declare module 'next-page-transitions'
-declare module 'react-transition-group'
-declare module 'react-slidy'
-declare module 'react-scroll-horizontal'
+declare module "@sanity/block-content-to-html";
+declare module "next-page-transitions";
+declare module "react-transition-group";
+declare module "react-slidy";
+declare module "react-scroll-horizontal";
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,48 +1,35 @@
-import { NextPage } from 'next'
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import { HomeFeed } from '../components/HomeProjectGrid'
-import { ProjectThumbnail } from '../components/HomeProjectThumbnail'
-import { Layout } from '../components/Layout'
-import { client, SanityImage, urlFor } from '../lib/sanity'
+import { NextPage } from "next";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { HomeFeed } from "../components/HomeProjectGrid";
+import { ProjectThumbnail } from "../components/HomeProjectThumbnail";
+import { Layout } from "../components/Layout";
+import { client, SanityImage, urlFor } from "../lib/sanity";
 import {
   FilterLink,
   NavigationFilterLinks,
-} from '../components/NavigationFilterLinks'
+} from "../components/NavigationFilterLinks";
 
-const OVERVIEW_SLUG = 'overview'
+const OVERVIEW_SLUG = "overview";
 
 const getThumbnail = (img: SanityImage) => {
-  const THUMB_IMAGE_WIDTH = 960
-  const imageUrl = urlFor(img).width(THUMB_IMAGE_WIDTH).url() || ''
-  return imageUrl
-}
+  const THUMB_IMAGE_WIDTH = 960;
+  const imageUrl = urlFor(img).width(THUMB_IMAGE_WIDTH).url() || "";
+  return imageUrl;
+};
 
 const Home: NextPage<{
-  data: {
-    overview: SanityImage[]
-    categories?: {
-      overview?: boolean
-      projects?: {
-        featured?: SanityImage
-        _id?: string
-        title?: string
-        slug?: { current?: string }
-      }[]
-      slug: string
-      title: string
-    }[]
-  }
+  data: {};
 }> = ({ data }) => {
-  const overviewImgs = data.overview
-  const categories = [...(data.categories || [])]
-  const router = useRouter()
-  const slug = router.query.filter || OVERVIEW_SLUG
-  const [currentCategorySlug, setCurrentCategorySlug] = useState(slug)
+  const overviewImgs = data.overview;
+  const categories = [...(data.categories || [])];
+  const router = useRouter();
+  const slug = router.query.filter || OVERVIEW_SLUG;
+  const [currentCategorySlug, setCurrentCategorySlug] = useState(slug);
 
   const currentCategory = categories?.find(
     (cat) => cat.slug === currentCategorySlug
-  )
+  );
 
   return (
     <Layout
@@ -61,8 +48,8 @@ const Home: NextPage<{
               onClick={() => setCurrentCategorySlug(category.slug)}
               isCurrent={category.slug === currentCategorySlug}
               key={category.slug}
-              slug={category.slug || 'unknown'}
-              title={category.title || ' Unknown Title'}
+              slug={category.slug || "unknown"}
+              title={category.title || " Unknown Title"}
             />
           ))}
         </NavigationFilterLinks>
@@ -81,16 +68,16 @@ const Home: NextPage<{
           ))}
         {currentCategory?.projects?.map((project, i) => (
           <ProjectThumbnail
-            name={project.title || 'Untitled Project'}
+            name={project.title || "Untitled Project"}
             key={project._id}
-            slug={project?.slug?.current || '<blank-slug>'}
+            slug={project?.slug?.current || "<blank-slug>"}
             imageUrl={getThumbnail(project.featured)}
           />
         ))}
       </HomeFeed>
     </Layout>
-  )
-}
+  );
+};
 
 export async function getServerSideProps() {
   const data = await client.fetch(`
@@ -102,11 +89,11 @@ export async function getServerSideProps() {
         'projects': collections[]->
         }
     }
-  `)
+  `);
 
   return {
     props: { data }, // will be passed to the page component as props
-  }
+  };
 }
 
-export default Home
+export default Home;
